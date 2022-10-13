b4f416b1c98e68eca606882413c11eda2947f0cd
pyh1987 <pyh1987@naver.com>
2022-10-14 00:32:26 +0900

    useState able counter

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
-import {SafeAreaView, Text, Button} from 'react-native';
-import Box from './components/Box';
+import Counter from './components/Counter';
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
-  const onPress = () => {
-    setVisible(!visible);
-  };
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
   return (
-    <SafeAreaView>
-      <Button title="토글" onPress={onPress} />
-      <Box rounded={visible} size="large" color="blue" />
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+    backgroundColor: 'cyan',
+  },
+});
+
 export default App;
---
+++ App2.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+import {SafeAreaView, Text, Button} from 'react-native';
+import Box from './components/Box';
+
+const App2 = () => {
+  const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
+  return (
+    <SafeAreaView>
+      <Button title="토글" onPress={onPress} />
+      <Box rounded={visible} size="large" color="blue" />
+    </SafeAreaView>
+  );
+};
+export default App2;
